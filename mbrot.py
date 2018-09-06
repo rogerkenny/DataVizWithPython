27e693e1e1584deeeb472d64219c13b400e626d6
Roger Kenny <roger.kenny@dowjones.com>
2018-09-06 17:25:26 -0400

    Refactored to draw more effiecient colors

--- mbrot.py
+++ mbrot.py
@@ -14,24 +14,59 @@ def mandelbrotGenerator(C, i, rule):
 		yield Z
 		j += 1
 
-def algorithm(generator, width, height, rule, iterations, dw, dh, center, palette, fill):
+def algorithm(generator, width, height, rule, iterations, dw, dh, center):
 	wOffset, hOffset = width//-2, height//-2
-	output = ''
+	output = []
 	for h in range(height):
+		row = []
 		for w in range(width):
 			C = (((w + wOffset) * dw) + ((h + hOffset) * dh)) + center
 			i = 0
 			for z in generator(C, iterations, rule):
 				if abs(z) >= 20:
-					output += palette[i%len(palette)]
+					row.append(i)
 					break
 				i += 1
 			else:
 				# Stayed small. Mark as part of the set
-				output += fill
-		output += "\n"
+				row.append(-1)
+		output.append(row)
 
 	return output
+
+def colorStringFromList(l):
+	# Colors http://web.archive.org/web/20131009193526/http://bitmote.com/index.php?post/2012/11/19/Using-ANSI-Color-Codes-to-Colorize-Your-Bash-Prompt-on-Linux
+	colors = [196+24, 196+12, 160 +9, 124+1, 16+5]
+	def c(x):
+		if x < 0:
+			return '\033[48;5;0m'
+		else:
+			return '\033[48;5;{}m'.format(colors[x%len(colors)]) 
+
+	out = []
+	lastNum = None
+	for n in l:
+		if n != lastNum:
+			if len(out) != 0:
+				#terminate last color
+				out.append('\033[m')
+			out += c(n)
+			lastNum = n
+		out.append(' ')
+	out.append('\033[m')
+	return ''.join(out)
+
+def asciiStringFromList(l):
+	palette = "*=:-."
+	def a(x):
+		if x < 0:
+			return "█"
+		else:
+			return palette[x%len(palette)]
+	out = []
+	for n in l:
+		out.append(a(n))
+	return ''.join(out)
 
 
 def generate(f):
@@ -43,13 +78,6 @@ def generate(f):
 
 		realMin, realMax = -2.0+0j, 1.0+0j
 		imagMin, imagMax = 0-1.0j, 0+1.0j
-
-		# Colors http://web.archive.org/web/20131009193526/http://bitmote.com/index.php?post/2012/11/19/Using-ANSI-Color-Codes-to-Colorize-Your-Bash-Prompt-on-Linux
-
-		colors = [196+24, 196+12, 160 +9, 124+1, 16+5]
-
-		palette = "*=:-." if not color else [ '\033[48;5;{}m \033[m'.format(x) for x in colors ]
-		fill = "█" if not color else '\033[48;5;0m \033[m'
 
 		iterations = 100
 
@@ -86,12 +114,17 @@ def generate(f):
 		except Exception as e:
 			center = -0.50+0.0j
 
+		renderer = colorStringFromList if color else asciiStringFromList
+
 		# useRule
 		try:
-			print(f(generator, width, height, rules[useRule], iterations, dw, dh, center, palette, fill))
+			print('\n'.join( list(map( renderer, f(generator, width, height, rules[useRule], iterations, dw, dh, center)))))
+			print("\n")
 		except:
 			for r in rules:
-				print(f(generator, width, height, r, iterations, dw, dh, center, palette, fill))
+				print('\n'.join( list(map( renderer, f(generator, width, height, r, iterations, dw, dh, center)))))
+				print("\n")
+
 
 	return decoratedAlgorithm
 
@@ -123,3 +156,17 @@ zoomcenter = -0.7269+0.1889j
 makeMandelbrot()
 color = True
 makeMandelbrot()
+
+zoom = 10
+zoomcenter = -0.4+0.6j
+makeMandelbrot()
+
+zoomcenter = -0.8j
+makeMandelbrot()
+
+zoomcenter = -1.8
+makeMandelbrot()
+color = False
+makeMandelbrot()
+
+
